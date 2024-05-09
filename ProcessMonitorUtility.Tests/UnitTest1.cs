7648e7f9c4b83f372b079a8569e24457b2026999
tudor129 <pirsantudor@yahoo.com>
2024-05-09 14:42:17 +0300

    Initial commit.

---
+++ ProcessMonitorUtility.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace ProcessMonitorUtility.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
